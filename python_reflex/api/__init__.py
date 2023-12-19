46e119415392782cda1814157050f6691e59bd67
Sarctiann (Sebastian Rodriguez) <sebastian.rodriguez@hstpathways.com>
2023-12-19 13:01:33 -0300

    wip

--- python_reflex/api/__init__.py
+++ python_reflex/api/__init__.py
@@ -4,10 +4,10 @@ from fastapi.responses import JSONResponse
 __all__ = ["init_api"]
 
 
-async def handle_request() -> JSONResponse:
+async def handle_request(api_route: str) -> JSONResponse:
     # TODO: Implement the ApiAtate to get the handler fbased on the url
-    return JSONResponse({"message": "Hello World"}, 200)
+    return JSONResponse({"message": "Hello World", "request_url": api_route}, 200)
 
 
 def init_api(app: rx.App) -> None:
-    app.api.add_api_route("/[api_route]", handle_request)
+    app.api.add_api_route("/{api_route}", handle_request)
--- rxconfig.py
+++ rxconfig.py
@@ -3,4 +3,5 @@ import reflex as rx
 config = rx.Config(
     app_name="python_reflex",
     frontend_port=3001,
+    backend_port=8080,
 )
